83c3ab319d38420ab1d84415dfa58b5d9244ec1c
francisco cuesta <cuestafrancisco@gmail.com>
2020-12-09 11:33:12 -0500

    imported Form, Titles and Weather componets into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
